7ea501b9a18974112d825d78814c0791cfad12ed
Peter Lin <peter@fractional.app>
2024-07-26 12:31:31 -0400

    Fix insert issue with decorators on empty state (#6)

--- Lexical/Core/Selection/RangeSelection.swift
+++ Lexical/Core/Selection/RangeSelection.swift
@@ -527,6 +527,10 @@ public class RangeSelection: BaseSelection {
       try removeText()
     }
 
+    if isRootNode(node: try anchor.getNode()) {
+      try removeText()
+    }
+
     let anchor = anchor
     let anchorOffset = anchor.offset
     let anchorNode = try anchor.getNode()
@@ -583,7 +587,6 @@ public class RangeSelection: BaseSelection {
     var didReplaceOrMerge = false
 
     for node in nodes {
-
       if let node = node as? DecoratorNode {
         if node == firstNode && node.isTopLevel() {
           if let unwrappedTarget = target as? ElementNode,
